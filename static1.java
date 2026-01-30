852c6913a1e72b82839487bcee435e37e3fbb9ba
gaursatyam71-sudo <gaursatyam71@gmail.com>
2026-01-30 13:17:00 +0530

    Add files via upload

---
+++ static1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class static1 {
+    //static int n;
+    int n;
+    public static void main(String[]args){
+        Scanner s= new Scanner(System.in);
+        static1 m=new static1();
+        //int n;
+        m.n=s.nextInt();
+       // n=s.nextInt();//
+        //System.out.println(n);//
+        System.out.println(m.n);
+    }    
+}
---
+++ static2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class static2 {
+   // static int n;
+   // int n;
+    public static void main(String[]args){
+        Scanner s= new Scanner(System.in);
+       // static2 m=new static2();
+        int n;
+       // m.n=s.nextInt();
+        n=s.nextInt();
+        System.out.println(n);
+       // System.out.println(m.n);
+    }    
+}
+
